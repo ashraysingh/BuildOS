85716a613aef40e1f361b8ca997d2ae0d16e9d61
ashray <ashraykrsingh123@gmail.com>
2023-07-09 23:44:26 +0530

    haydn:build legion | C

--- build.sh
+++ build.sh
@@ -1,24 +1,19 @@
 # Normal build steps
 . build/envsetup.sh
-lunch arrow_lavender-userdebug
+lunch legion_haydn-userdebug
+
+build_gapps=1
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export BUILD_BROKEN_DUP_RULES=true
 export RELAX_USES_LIBRARY_CHECK=true
+export LEGION_GAPPS=false
 #export TARGET_BOOTANIMATION_RES=1080
 
-#build_gapps=1
-#export ARROW_GAPPS=true
-#export WITH_GMS=false
-#export WITH_GAPPS=false
-
 exp_gapps() {
-export ARROW_GAPPS=true
-export WITH_GMS=true
-export WITH_GAPPS=true
-export BLISS_BUILD_VARIANT=gapps
+export LEGION_GAPPS=true
 }
 
 compile_plox () {
