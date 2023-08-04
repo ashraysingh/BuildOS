18226adac1024482f628f196ffbbf6dbfbc11a4b
wHo-EM-i <ehteshammalik18998@gmail.com>
2023-08-04 21:14:08 +0530

    ci: PE+ 4.19

--- build.sh
+++ build.sh
@@ -1,20 +1,19 @@
 # Normal build steps
 . build/envsetup.sh
-lunch lineage_lavender-userdebug
+lunch aosp_lavender-userdebug
 
 # export variable here
 export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
+export WITH_GMS=true
+export WITH_GAPPS=true
 #export BUILD_BROKEN_DUP_RULES=true
 export RELAX_USES_LIBRARY_CHECK=true
 #export TARGET_BOOTANIMATION_RES=1080
-export LINEAGE_BUILDTYPE=COMMUNITY
 
 #build_gapps=1
-export ARROW_GAPPS=false
-export WITH_GMS=false
-export WITH_GAPPS=false
-export USE_GAPPS=false
+#export ARROW_GAPPS=false
+#export USE_GAPPS=false
 
 exp_gapps() {
 export TARGET_GAPPS_ARCH=arm64
@@ -22,7 +21,7 @@ export USE_GAPPS=true
 export ARROW_GAPPS=true
 export WITH_GMS=true
 export WITH_GAPPS=true
-export BLISS_BUILD_VARIANT=gapps 
+export BLISS_BUILD_VARIANT=gapps
 }
 
 compile_plox () {
